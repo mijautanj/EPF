fee637cda2db976474798e6b7007dc1be8f4c30a
mijautanj <amzdybek@gmail.com>
2021-05-27 12:09:15 +0200

    finished mlp

--- models/MLP.py
+++ models/MLP.py
@@ -3,14 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #Keras imports
-from tensorflow.keras.layers import Dropout, Dense, LSTM
+from tensorflow.keras.layers import Dropout, Dense, BatchNormalization, Activation
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import LSTM
 
 #Helpfunctions imports
-from helpFunc.sequenceData import obtainDataDict, extractValuesLSTM
+from helpFunc.sequenceData import obtainDataDict, extractValuesMLP
 from helpFunc.ancillaryFunctions import evaluate, minMaxLoss
 from helpFunc.metrics import MAE, sMAPE, MAPE, RMSE
 from helpFunc.plots import plotLossFunction,plotWorstBest, plotAllPred
@@ -19,12 +18,14 @@ from helpFunc.plots import plotLossFunction,plotWorstBest, plotAllPred
 
 
 # MNIST class
-class LSTM_class():
+class MLP_class():
     def __init__(self,
                  n_hidden1, 
                  n_hidden2,
+                 n_hidden3,
                  dropout1,
                  dropout2,
+                 dropout3,
                  batch_size,
                  lossMetric,
                  epochs,
@@ -39,11 +40,13 @@ class LSTM_class():
                  plotWorstBestPrediction,
                  plotAllPredictions):
 
-        self.modelName="LSTM"
+        self.modelName="MLP"
         self.n_hidden1 = n_hidden1
         self.n_hidden2 = n_hidden2
+        self.n_hidden3 = n_hidden3
         self.dropout1 = dropout1
         self.dropout2 = dropout2
+        self.dropout3 = dropout3
         self.batch_size = batch_size
         self.epochs = epochs
         self.learningRate = learningRate
@@ -57,32 +60,46 @@ class LSTM_class():
         self.plotLoss=plotLoss
         self.plotWorstBestPrediction=plotWorstBestPrediction
         self.plotAllPredictions=plotAllPredictions
-        self.__x_train, self.__x_val, self.__x_test, self.__y_train, self.__y_val, self.__y_test, self.targetScaler, self.dataDict, self.df = self.lstm_data()
-        self.__model = self.lstm_model()
+        self.__x_train, self.__x_val, self.__x_test, self.__y_train, self.__y_val, self.__y_test, self.targetScaler, self.dataDict, self.df = self.mlp_data()
+        self.__model = self.mlp_model()
         
         
     # load data from sequenceData
-    def lstm_data(self):
+    def mlp_data(self):
         PRICEAREA = self.priceArea
         TARGETNAME = self.targetName
         df, dataDict, targetScaler = obtainDataDict(PRICEAREA, TARGETNAME, weeklySequence=self.weeklySequence, dailySequence=self.dailySequence)
-        X_train, y_train = extractValuesLSTM(dataDict["train"][0],dataDict["train"][1],TARGETNAME)
-        X_val, y_val = extractValuesLSTM(dataDict["val"][0],dataDict["val"][1],TARGETNAME)
-        X_test, y_test = extractValuesLSTM(dataDict["test"][0],dataDict["test"][1],TARGETNAME)
+        X_train, y_train = extractValuesMLP(dataDict["train"][0],dataDict["train"][1],TARGETNAME)
+        X_val, y_val = extractValuesMLP(dataDict["val"][0],dataDict["val"][1],TARGETNAME)
+        X_test, y_test = extractValuesMLP(dataDict["test"][0],dataDict["test"][1],TARGETNAME)
         return X_train, X_val, X_test, y_train, y_val, y_test, targetScaler, dataDict, df
     
     # model
-    def lstm_model(self):        
-        n_steps = self.__x_train.shape[1]
-        n_features = self.__x_train.shape[2]
+    def mlp_model(self):        
+        n_features = self.__x_train.shape[1]
         n_output = self.__y_train.shape[1]
 
         model = Sequential()
-        model.add(LSTM(self.n_hidden1, input_shape=(n_steps, n_features), return_sequences=True))
+
+        #First layer
+        model.add(Dense(self.n_hidden1, input_dim=n_features))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
         model.add(Dropout(self.dropout1))
-        model.add(LSTM(self.n_hidden2, return_sequences=False))
+        
+        #Second layer
+        model.add(Dense(self.n_hidden2))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
         model.add(Dropout(self.dropout2))
 
+        #Third layer
+        model.add(Dense(self.n_hidden3))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Dropout(self.dropout3))
+
+        #Output layer
         model.add(Dense(n_output))
         opt = Adam(learning_rate=self.learningRate)
         model.compile(optimizer=opt, loss=self.lossMetric, metrics=self.lossMetric)
@@ -90,7 +107,7 @@ class LSTM_class():
     
 
     # fit lstm model
-    def lstmFit(self):
+    def mlpFit(self):
         early_stopping = EarlyStopping(patience=self.patience, verbose=1)
         fittedModel = self.__model.fit(self.__x_train, self.__y_train,
                        batch_size=self.batch_size,
@@ -102,10 +119,10 @@ class LSTM_class():
         if self.plotLoss:
             plotLossFunction(fittedModel)
 
-    # evaluate lstm model
-    def lstmPredict(self):
+    # evaluate mlp model
+    def mlpPredict(self):
         #Fitting (training) the model
-        self.lstmFit()
+        self.mlpFit()
        
         #Forecasting for all test samples
         y_hat_test = self.__model.predict(self.__x_test, batch_size=self.batch_size)
@@ -118,7 +135,7 @@ class LSTM_class():
             j["Target"] = self.targetScaler.inverse_transform(np.array(j["Scaled-target"]).reshape(-1,1))
 
 
-    def lstmEvaluate(self):
+    def mlpEvaluate(self):
         testMAE, errors = evaluate(self.dataDict["test"][1])
         print(errors)
         if self.plotWorstBestPrediction:
@@ -138,7 +155,7 @@ if __name__ == "__main__":
         'verboseTraining': 1,
         'plotLoss': True,
         'plotWorstBestPrediction': True,
-        'plotAllPredictions': False,
+        'plotAllPredictions': True,
 
         #Data parameters
         'priceArea' : 'SE1',
@@ -147,10 +164,12 @@ if __name__ == "__main__":
         'weeklySequence': True,
 
         #Network/training parameters
-        'n_hidden1': 64,
-        'n_hidden2': 64,
-        'dropout1': 0.2,
-        'dropout2': 0.2,
+        'n_hidden1': 1024,
+        'n_hidden2': 1024,
+        'n_hidden3': 512,
+        'dropout1': 0.3,
+        'dropout2': 0.3,
+        'dropout3': 0.3,
         'batch_size': 32,
         'epochs': 1000,
         'learningRate': 0.0001,
@@ -159,6 +178,6 @@ if __name__ == "__main__":
    
     }
 
-    _lstm = LSTM_class(**parameters)
-    _lstm.lstmPredict()
-    _lstm.lstmEvaluate()
+    _mlp = MLP_class(**parameters)
+    _mlp.mlpPredict()
+    _mlp.mlpEvaluate()
